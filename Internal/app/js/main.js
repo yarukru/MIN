bd360c0d6c8113bed859363b4b129bf2fdee3b21
Web_line <ekuksa1709@gmail.com>
2021-11-06 20:10:08 +0200

    Added new page errors.html, settings.html, news-all.html, news.html

--- Internal/app/js/main.js
+++ Internal/app/js/main.js
@@ -78,4 +78,91 @@ $(document).ready(function(){
         $(this).parent().parent().parent().parent().children('.content-promo').slideToggle(150);
     });
     // End Promo page //
+
+    // MODAL //
+    $(document).on('click', '.modal_w', function (e) {
+        e.preventDefault();
+        $( $.attr(this, 'href') ).fadeIn(200);
+    });
+    $(document).on('click', '.closed-modal', function (e) {
+        e.preventDefault();
+        $('.modal').fadeOut(200);
+    });
+    // END Modal//
+
+    // Pin Code //
+    $('.pin-code .keyboard .btn-keyboard').click(function(e) {
+        e.preventDefault();
+
+        const $block = $(this).closest('.pin-code');
+        const $result_mask_block = $block.find('.pin-field .box-mask');
+        const $result_input = $block.find('.pin-field .result_input');
+        const $result_input_value = $result_input.val();
+
+        const pin_length = parseInt($block.attr('data-value-length')) || 4;
+        const key_value = $(this).attr('data-value');
+
+
+        $result_mask_block.removeClass('invalid');
+        switch ($(this).attr('data-action')) {
+            case 'backspace':
+                $result_input.val($result_input_value.substr(0, $result_input_value.length - 1));
+                break;
+
+            case 'check':
+                // Demo check
+                if ($result_input_value === '1234' || $result_input_value.length < pin_length) {
+                    // invalid pin
+                    $result_input.val('');
+
+                    // Set invalid class after some time to rerun CSS animation
+                    setTimeout(() => $result_mask_block.addClass('invalid'), 10)
+                } else {
+                    // valid pin
+                    alert('Valid!');
+                }
+                break;
+
+            default:
+                if ($result_input_value.length < pin_length) {
+                    $result_input.val(`${$result_input_value}${key_value}`);
+                }
+                break
+        }
+
+
+        const $result_input_value_after = $result_input.val();
+        const mask_items = $result_mask_block.children('span')
+        mask_items.removeClass('active');
+        for (let index = 0; index < $result_input_value_after.length; index++) {
+            mask_items.eq(index).addClass('active');
+        }
+    });
+    // End Pin Code //
+
+    // MODAL Test Alert//
+    $(document).on('click', '.modal_alert_w', function (e) {
+        e.preventDefault();
+        $( $.attr(this, 'href') ).fadeIn(200);
+    });
+    $(document).on('click', '.closed-alert-modal', function (e) {
+        e.preventDefault();
+        $('.modal-alert').fadeOut(200);
+    });
+    // END Modal Test Alert//
+
+    // Pin Code wrap //
+    $(".pin-digit").on("keyup", function (e) {
+        if (e.keyCode == 8 || e.keyCode == 48) {
+            $(e.currentTarget).prev().select();
+            $(e.currentTarget).prev().focus();
+        } else {
+            if ($(e.currentTarget).val() != "") {
+                console.log($(e.currentTarget).next())
+                $(e.currentTarget).next().select();
+                $(e.currentTarget).next().focus();
+            }
+        }
+    });
+    // End Pin Code wrap //
 });
